5b0cf4bd9b60c150fae8e33e89c8715366929eba
María Teresa Obando Jiménez <maria.obando.jimenez@cuc.cr>
2024-07-29 16:52:28 -0600

    Se agregó la pantalla, estilo y el modelo del módulo Marcas -Pendiente: Controllador y conexion a BD

---
+++ Models/marcas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlSoft.Models
+{
+    public class marcas
+    {
+        public int idMarca { get; set; }
+        public int idEmpleado { get; set; }
+        public DateTime Fecha { get; set; }
+        public TimeSpan HoraEntrada { get; set; }
+        public TimeSpan HoraSalida { get; set; }
+    }
+}
